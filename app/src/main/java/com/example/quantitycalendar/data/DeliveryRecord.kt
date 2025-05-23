8c617567760fb363b545918ef213cb1d1e478fac
yongsulim <sulim.yong88@gmail.com>
2025-05-23 12:02:19 +0900

    Initial commit

--- app/src/main/java/com/example/quantitycalendar/data/DeliveryRecord.kt
+++
@@ -1,9 +0,0 @@
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "delivery_records")
-data class DeliveryRecord(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val date: String,  // 날짜 예: "2025-05-03"
-    val quantity: Int  // 배송한 수량
-)
